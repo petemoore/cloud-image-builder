ca13f8f7adcea7e5966975bb3869d01f72311aed
Rob Thijssen <rthijssen@gmail.com>
2018-07-20 13:52:47 +0300

    attempts to set s3 credentials

--- build_ami.ps1
+++ build_ami.ps1
@@ -10,7 +10,7 @@ if (-not (New-Object System.Security.Principal.WindowsPrincipal([System.Security
   $processStartInfo.UseShellExecute = $false
   $process = New-Object System.Diagnostics.Process
   $process.StartInfo = $processStartInfo
-  $process.Start()
+  $process.Start() | Out-Null
   $process.StandardOutput.ReadToEnd()
   $process.WaitForExit()
   if ($process.ExitCode) {
@@ -19,10 +19,48 @@ if (-not (New-Object System.Security.Principal.WindowsPrincipal([System.Security
   }
   exit
 }
-$iso_url='https://software-download.microsoft.com/pr/Win10_1803_EnglishInternational_x64.iso?t=080ddbfb-902c-4c57-beb8-bc1c57378ed5&e=1531303905&h=265b8b68c521570bc0fb860c7a5f3590'
-$iso_path='.\Win10_1803_EnglishInternational_x64.iso'
-$vhd_path='.\Win10_1803_EnglishInternational_x64.vhdx'
-(New-Object Net.WebClient).DownloadFile($iso_url, $iso_path)
-(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mozilla-platform-ops/relops_image_builder/master/Convert-WindowsImage.ps1', '.\Convert-WindowsImage.ps1')
-. .\Convert-WindowsImage.ps1
-Convert-WindowsImage -SourcePath $iso_path -VHDPath $vhd_path -Edition Core
+
+$image_name='Win10_1803_EnglishInternational_x64'
+$image_edition='Core'
+$image_capture_date=((Get-Date).ToUniversalTime().ToString('yyyyMMddHHmmss'))
+$image_key=('{0}-{1}-{2}' -f $image_name, $image_edition, $image_capture_date)
+
+$aws_region='us-west-2'
+$s3_bucket='windows-ami-builder'
+$s3_vhd_key=('vhd/{0}-{1}.vhdx' -f $image_name, $image_edition)
+
+$iso_url=('https://s3-{0}.amazonaws.com/{1}/iso/{2}.iso' -f $aws_region, $s3_bucket, $image_name)
+$iso_path=('.\{0}.iso' -f $image_name)
+
+$vhd_path=('.\{0}-{1}.vhdx' -f $image_name, $image_edition)
+
+Set-ExecutionPolicy RemoteSigned
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name AWSPowerShell
+
+$aws_creds=(Invoke-WebRequest -Uri 'http://169.254.169.254/latest/meta-data/iam/security-credentials/windows-ami-builder' -UseBasicParsing | ConvertFrom-Json)
+$env:AWS_ACCESS_KEY_ID = $aws_creds.AccessKeyId
+$env:AWS_SECRET_ACCESS_KEY = $aws_creds.SecretAccessKey
+$env:AWS_SESSION_TOKEN = $aws_creds.Token
+Set-AWSCredential -AccessKey $aws_creds.AccessKeyId -SecretKey $aws_creds.SecretAccessKey -StoreAs WindowsAmiBuilder
+Initialize-AWSDefaultConfiguration -ProfileName WindowsAmiBuilder -Region $aws_region
+
+# download the iso file if it is not on the local filesystem
+if (-not (Test-Path -Path $iso_path -ErrorAction SilentlyContinue)) {
+  (New-Object Net.WebClient).DownloadFile($iso_url, $iso_path)
+}
+
+if (-not (Test-Path -Path './Convert-WindowsImage.ps1' -ErrorAction SilentlyContinue)) {
+  (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mozilla-platform-ops/relops_image_builder/master/Convert-WindowsImage.ps1', '.\Convert-WindowsImage.ps1')
+}
+
+# create the vhd(x) file if it is not on the local filesystem
+if (-not (Test-Path -Path $vhd_path -ErrorAction SilentlyContinue)) {
+  . .\Convert-WindowsImage.ps1
+  Convert-WindowsImage -SourcePath $iso_path -VHDPath $vhd_path -Edition $image_edition
+}
+
+# upload the vhd(x) file if it is not in the s3 bucket
+if (-not (Get-S3Object -BucketName $s3_bucket -Key $s3_vhd_key -Region $aws_region)) {
+  Write-S3Object -BucketName $s3_bucket -File $vhd_path -Key $vhd_key
+}
