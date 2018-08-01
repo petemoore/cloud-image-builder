4a5ac55485bba6c5f66d3cd8060bfd524607ed72
Rob Thijssen <rthijssen@gmail.com>
2018-08-01 15:30:08 +0300

    resolve path to unattend

--- build_ami.ps1
+++ build_ami.ps1
@@ -107,7 +107,7 @@ if (-not (Test-Path -Path $ua_path -ErrorAction SilentlyContinue)) {
 if (-not (Test-Path -Path $vhd_path -ErrorAction SilentlyContinue)) {
   try {
     . .\Convert-WindowsImage.ps1
-    Convert-WindowsImage -SourcePath $iso_path -VhdPath $vhd_path -VhdFormat $vhd_format -VhdPartitionStyle $vhd_partition_style -Edition $image_edition -UnattendPath $ua_path
+    Convert-WindowsImage -SourcePath $iso_path -VhdPath $vhd_path -VhdFormat $vhd_format -VhdPartitionStyle $vhd_partition_style -Edition $image_edition -UnattendPath (Resolve-Path -Path $ua_path).Path
     Write-Host -object ('created {0} from {1}' -f $vhd_path, $iso_path) -ForegroundColor White
   } catch {
     Write-Host -object $_.Exception.Message -ForegroundColor Red
