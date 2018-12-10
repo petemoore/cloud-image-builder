37c44fd78b54b2a4583bba79b0b47798add4c5bf
grenade <rthijssen@gmail.com>
2018-12-10 08:51:04 +0200

    remove typo

--- build_ami.ps1
+++ build_ami.ps1
@@ -235,8 +235,6 @@ if (Test-Path -Path $vhd_path -ErrorAction SilentlyContinue) {
 # create the vhd(x) file
 try {
   . .\Convert-WindowsImage.ps1
-
-  BCDBoot
   if ($drivers.length) {
     if ($ec2_settings_map[$target_worker_type]['architecture'].Contains('arm')) {
       Convert-WindowsImage -SourcePath $iso_path -VhdPath $vhd_path -VhdFormat $config.format -VhdPartitionStyle $config.partition -Edition $config.edition -UnattendPath (Resolve-Path -Path $ua_path).Path -Driver $drivers -RemoteDesktopEnable:$true -BCDBoot ('{0}\System32\bcdboot.exe' -f $env:SystemRoot)
