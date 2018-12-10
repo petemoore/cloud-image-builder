41900c9f75ab7025215419bcd0c4c4f4e8291192
grenade <rthijssen@gmail.com>
2018-12-10 08:27:00 +0200

    use full rather than relative paths

--- build_ami.ps1
+++ build_ami.ps1
@@ -104,10 +104,11 @@ $aws_region = 'us-west-2'
 $aws_availability_zone = ('{0}c' -f $aws_region)
 
 $cwi_url = 'https://raw.githubusercontent.com/mozilla-platform-ops/relops-image-builder/master/Convert-WindowsImage.ps1'
-$cwi_path = ('.\{0}' -f [System.IO.Path]::GetFileName($cwi_url))
-$ua_path = ('.\{0}' -f [System.IO.Path]::GetFileName($config.unattend))
-$iso_path = ('.\{0}' -f [System.IO.Path]::GetFileName($config.iso.key))
-$vhd_path = ('.\{0}' -f [System.IO.Path]::GetFileName($config.vhd.key))
+$work_dir = (Resolve-Path -Path '.\').Path
+$cwi_path = (Join-Path -Path $work_dir -ChildPath ([System.IO.Path]::GetFileName($cwi_url)))
+$ua_path = (Join-Path -Path $work_dir -ChildPath ([System.IO.Path]::GetFileName($config.unattend)))
+$iso_path = (Join-Path -Path $work_dir -ChildPath ([System.IO.Path]::GetFileName($config.iso.key)))
+$vhd_path = (Join-Path -Path $work_dir -ChildPath ([System.IO.Path]::GetFileName($config.vhd.key)))
 
 Set-ExecutionPolicy RemoteSigned
 
