8038ff8494714200e4278f58470d9c085b7dc661
Rob Thijssen <rthijssen@gmail.com>
2019-01-02 15:47:57 +0200

    decision task experiment

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -4,7 +4,10 @@ import taskcluster
 #import taskcluster.aio
 import uuid
 
-queue = taskcluster.Queue({})
+options = {
+  rootUrl: 'https://taskcluster'
+}
+queue = taskcluster.Queue(options)
 #loop = asyncio.get_event_loop()
 #session = taskcluster.aio.createSession(loop=loop)
 #asyncQueue = taskcluster.aio.Queue(options, session=session)
