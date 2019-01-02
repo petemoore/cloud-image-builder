d21acb23908fd18df592df579b26dd497aa523bb
Rob Thijssen <rthijssen@gmail.com>
2019-01-02 14:50:33 +0200

    decision task experiment

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -1,11 +1,12 @@
-import asyncio
+#import asyncio
 import taskcluster
-import taskcluster.aio
+#import taskcluster.aio
 import uuid
 
-loop = asyncio.get_event_loop()
-session = taskcluster.aio.createSession(loop=loop)
-asyncQueue = taskcluster.aio.Queue(options, session=session)
+queue = taskcluster.Queue(options)
+#loop = asyncio.get_event_loop()
+#session = taskcluster.aio.createSession(loop=loop)
+#asyncQueue = taskcluster.aio.Queue(options, session=session)
 
 for i in range(0, 2):
   taskId=uuid.uuid4().hex
@@ -13,9 +14,10 @@ for i in range(0, 2):
     maxRunTime: 30,
     command: [
       'echo',
-      'i am task {}'.format(taskId)
+      '"i am task {}"'.format(taskId)
     ]
   }
-  await asyncQueue.createTask(taskId=taskId, payload=payload)
+  taskCreateResult = queue.createTask(taskId, payload)
+  #await asyncQueue.createTask(taskId=taskId, payload=payload)
 
 #await asyncQueue.listTaskGroup(taskGroupId='value') # -> result
