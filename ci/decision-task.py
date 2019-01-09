cb6eea03561dc9b719d3e239cecd1ffb671d0963
Rob Thijssen <rthijssen@gmail.com>
2019-01-09 11:59:55 +0200

    decision task corrections

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -3,20 +3,27 @@ import slugid
 import taskcluster
 from datetime import datetime, timedelta
 
-queue = taskcluster.Queue({
+options = {
+  'rootUrl': 'https://taskcluster.net',
+  'credentials': {
+    'clientId': os.environ.get('TASKCLUSTER_CLIENT_ID'),
+    'accessToken': os.environ.get('TASKCLUSTER_ACCESS_TOKEN')
+  }
+} if 'TASKCLUSTER_CLIENT_ID' in os.environ and 'TASKCLUSTER_ACCESS_TOKEN' in os.environ else {
   'rootUrl': 'https://taskcluster'
-})
+}
+queue = taskcluster.Queue(options)
 
 tasks = range(1, 3)
 for i in tasks:
-  taskId = slugid.nice()
+  taskId = slugid.nice().decode('utf-8')
   payload = {
     'created': '{}Z'.format(datetime.utcnow().isoformat()[:-3]),
     'deadline': '{}Z'.format((datetime.utcnow() + timedelta(days=3)).isoformat()[:-3]),
     'provisionerId': 'aws-provisioner-v1',
     'workerType': 'github-worker',
     'schedulerId': 'taskcluster-github',
-    'taskGroupId': os.environ.get('TASK_ID'),
+    'taskGroupId': os.getenv('TASK_ID', taskId),
     'routes': [
       'project.releng.relops-image-builder.v1.revision.{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
     ],
@@ -35,15 +42,15 @@ for i in tasks:
       ],
       'features': {
         'taskclusterProxy': True
-      },
-      'metadata': {
-        'name': 'task {} ({})'.format(i, taskId),
-        'description': 'description of task {} ({})'.format(i, taskId),
-        'owner': os.environ.get('GITHUB_HEAD_USER_EMAIL'),
-        'source': 'https://github.com/mozilla-platform-ops/relops-image-builder/commit/{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
       }
+    },
+    'metadata': {
+      'name': 'task {} ({})'.format(i, taskId),
+      'description': 'description of task {} ({})'.format(i, taskId),
+      'owner': os.getenv('GITHUB_HEAD_USER_EMAIL', 'grenade@mozilla.com'),
+      'source': 'https://github.com/mozilla-platform-ops/relops-image-builder/commit/{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
     }
   }
-  print('creating task {}/{} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(i, tasks[-1], os.environ.get('TASK_ID'), taskId))
+  print('creating task {}/{} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(i, tasks[-1], os.getenv('TASK_ID', taskId), taskId))
   taskStatusResponse = queue.createTask(taskId, payload)
   print(taskStatusResponse)
