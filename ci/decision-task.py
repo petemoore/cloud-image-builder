9a1e084d644b41722acf4828c04111f4972d9328
Rob Thijssen <rthijssen@gmail.com>
2019-01-09 17:48:37 +0200

    decision task: simplify local run

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -3,27 +3,16 @@ import slugid
 import taskcluster
 from datetime import datetime, timedelta
 
-options = {
-  'rootUrl': 'https://taskcluster.net',
-  'credentials': {
-    'clientId': os.environ.get('TASKCLUSTER_CLIENT_ID'),
-    'accessToken': os.environ.get('TASKCLUSTER_ACCESS_TOKEN')
-  }
-} if 'TASKCLUSTER_CLIENT_ID' in os.environ and 'TASKCLUSTER_ACCESS_TOKEN' in os.environ else {
-  'rootUrl': 'http://taskcluster'
-}
-queue = taskcluster.Queue(options)
-
-tasks = range(1, 3)
-for i in tasks:
-  taskId = slugid.nice().decode('utf-8')
+queue = taskcluster.Queue({'rootUrl': os.getenv('TASKCLUSTER_ROOT_URL', 'https://taskcluster')})
+taskIds = [slugid.nice().decode('utf-8') for i in range(1, 3)]
+for taskId in taskIds:
   payload = {
     'created': '{}Z'.format(datetime.utcnow().isoformat()[:-3]),
     'deadline': '{}Z'.format((datetime.utcnow() + timedelta(days=3)).isoformat()[:-3]),
     'provisionerId': 'aws-provisioner-v1',
     'workerType': 'github-worker',
     'schedulerId': 'taskcluster-github',
-    'taskGroupId': os.getenv('TASK_ID', taskId),
+    'taskGroupId': os.getenv('TASK_ID', taskIds[0]),
     'routes': [
       'project.releng.relops-image-builder.v1.revision.{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
     ],
@@ -45,12 +34,12 @@ for i in tasks:
       }
     },
     'metadata': {
-      'name': 'task {} ({})'.format(i, taskId),
-      'description': 'description of task {} ({})'.format(i, taskId),
+      'name': 'task {}'.format(taskId),
+      'description': 'description of task {}'.format(taskId),
       'owner': os.getenv('GITHUB_HEAD_USER_EMAIL', 'grenade@mozilla.com'),
       'source': 'https://github.com/mozilla-platform-ops/relops-image-builder/commit/{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
     }
   }
-  print('creating task {}/{} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(i, tasks[-1], os.getenv('TASK_ID', taskId), taskId))
+  print('creating task {} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(taskId, os.getenv('TASK_ID', taskIds[0]), taskId))
   taskStatusResponse = queue.createTask(taskId, payload)
   print(taskStatusResponse)
