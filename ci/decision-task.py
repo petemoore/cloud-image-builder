e4cb244bdfe236b56f73492ac4345964b2a9257f
Rob Thijssen <rthijssen@gmail.com>
2019-05-29 16:18:34 +0300

    Bug 1553572 - disable notification center on win 10

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -6,12 +6,12 @@ from datetime import datetime, timedelta
 queue = taskcluster.Queue({'rootUrl': os.getenv('TASKCLUSTER_PROXY_URL', os.getenv('TASKCLUSTER_ROOT_URL'))})
 targets = [
   {
-    workerType: 'gecko-t-win10-64-alpha',
-    provisioner: 'aws-provisioner-v1'
+    'workerType': 'gecko-t-win10-64-alpha',
+    'provisioner': 'aws-provisioner-v1'
   },
   {
-    workerType: 'gecko-t-win10-64-gpu-a',
-    provisioner: 'aws-provisioner-v1'
+    'workerType': 'gecko-t-win10-64-gpu-a',
+    'provisioner': 'aws-provisioner-v1'
   }
 ]
 for target in targets:
@@ -19,7 +19,7 @@ for target in targets:
   payload = {
     'created': '{}Z'.format(datetime.utcnow().isoformat()[:-3]),
     'deadline': '{}Z'.format((datetime.utcnow() + timedelta(days=3)).isoformat()[:-3]),
-    'provisionerId': target.provisioner,
+    'provisionerId': target['provisioner'],
     'workerType': 'relops-image-builder',
     'schedulerId': 'taskcluster-github',
     'taskGroupId': os.environ.get('TASK_ID'),
@@ -39,7 +39,7 @@ for target in targets:
         'git clone {} relops-image-builder'.format(os.environ.get('GITHUB_HEAD_REPO_URL')),
         'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder config advice.detachedHead false',
         'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder checkout {}'.format(os.environ.get('GITHUB_HEAD_SHA')),
-        'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\build_ami.ps1 {}'.format(target.workerType)
+        'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\build_ami.ps1 {}'.format(target['workerType'])
       ],
       'features': {
         'runAsAdministrator': True,
@@ -47,8 +47,8 @@ for target in targets:
       }
     },
     'metadata': {
-      'name': 'iso-to-ami {}'.format(target.workerType),
-      'description': 'build windows ami from iso for {}'.format(target.workerType),
+      'name': 'iso-to-ami {}'.format(target['workerType']),
+      'description': 'build windows ami from iso for {}'.format(target['workerType']),
       'owner': os.environ.get('GITHUB_HEAD_USER_EMAIL'),
       'source': '{}/commit/{}'.format(os.environ.get('GITHUB_HEAD_REPO_URL'), os.environ.get('GITHUB_HEAD_SHA'))
     }
