2588d59b754bca466e56f1811e791c61c8e861fb
Rob Thijssen <rthijssen@gmail.com>
2019-10-30 16:23:15 +0200

    only build ami/image if unattend file has changed

--- ci/decision-task.py
+++ ci/decision-task.py
@@ -1,3 +1,4 @@
+import json
 import os
 import slugid
 import taskcluster
@@ -188,61 +189,84 @@ targets = [
     'decription': 'build windows server 2019 google cloud image from iso'
   }
 ]
+changedFiles = os.environ.get('git_changed_files').split('\n')
+print(changedFiles)
+
 for target in targets:
-  payload = {
-    'created': '{}Z'.format(datetime.utcnow().isoformat()[:-3]),
-    'deadline': '{}Z'.format((datetime.utcnow() + timedelta(days=3)).isoformat()[:-3]),
-    'provisionerId': target['builder']['workerPool'],
-    'workerType': target['builder']['workerType'],
-    'schedulerId': 'taskcluster-github',
-    'taskGroupId': os.environ.get('TASK_ID'),
-    'routes': [
-      'index.project.releng.relops-image-builder.v1.revision.{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
-    ],
-    'scopes': [
-      'generic-worker:os-group:{}/{}/Administrators'.format(target['builder']['workerPool'], target['builder']['workerType']),
-      'generic-worker:run-as-administrator:{}/{}'.format(target['builder']['workerPool'], target['builder']['workerType'])
-    ],
-    'payload': {
-      'osGroups': [
-        'Administrators'
-      ],
-      'maxRunTime': 10800,
-      'artifacts': [
-        {
-          "name": "public/screenshot",
-          "path": "public/screenshot",
-          "type": "directory"
+  with open('./relops-image-builder/worker-type-map.json') as mapFile:
+    wtMap = json.load(mapFile)
+  with open('./relops-image-builder/manifest.json') as manifestFile:
+    manifest = json.load(manifestFile)
+
+  manifestItem = next((x for x in manifest if (x['os'] == 'Windows'
+    and x['build']['major'] == wtMap[target['workerType']]['build']['major']
+    and x['build']['release'] == wtMap[target['workerType']]['build']['release']
+    and x['build']['build'] == wtMap[target['workerType']]['build']['build']
+    and x['version'] == wtMap[target['workerType']]['version']
+    and x['edition'] == wtMap[target['workerType']]['edition']
+    and x['language'] == wtMap[target['workerType']]['language']
+    and x['architecture'] == wtMap[target['workerType']]['architecture'])), None)
+
+  if manifestItem is not None:
+    if (target['provider'] == 'ec2' and 'unattend/{}'.format(os.path.basename(manifestItem['unattend'])) in changedFiles) or (target['provider'] == 'gcp' and 'unattend/gcp/{}'.format(os.path.basename(manifestItem['unattend'])) in changedFiles):
+      payload = {
+        'created': '{}Z'.format(datetime.utcnow().isoformat()[:-3]),
+        'deadline': '{}Z'.format((datetime.utcnow() + timedelta(days=3)).isoformat()[:-3]),
+        'provisionerId': target['builder']['workerPool'],
+        'workerType': target['builder']['workerType'],
+        'schedulerId': 'taskcluster-github',
+        'taskGroupId': os.environ.get('TASK_ID'),
+        'routes': [
+          'index.project.releng.relops-image-builder.v1.revision.{}'.format(os.environ.get('GITHUB_HEAD_SHA'))
+        ],
+        'scopes': [
+          'generic-worker:os-group:{}/{}/Administrators'.format(target['builder']['workerPool'], target['builder']['workerType']),
+          'generic-worker:run-as-administrator:{}/{}'.format(target['builder']['workerPool'], target['builder']['workerType'])
+        ],
+        'payload': {
+          'osGroups': [
+            'Administrators'
+          ],
+          'maxRunTime': 10800,
+          'artifacts': [
+            {
+              "name": "public/screenshot",
+              "path": "public/screenshot",
+              "type": "directory"
+            }
+          ] if target['provider'] == 'ec2' else [],
+          'command': [
+            'git clone {} relops-image-builder'.format(os.environ.get('GITHUB_HEAD_REPO_URL')),
+            'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder config advice.detachedHead false',
+            'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder checkout {}'.format(os.environ.get('GITHUB_HEAD_SHA')),
+            'for /F "delims=" %%i in (\'"C:\\Program Files (x86)\\Google\\Cloud SDK\\google-cloud-sdk\\bin\\gcloud.cmd" components copy-bundled-python\') do (set CLOUDSDK_PYTHON=%%i)',
+            'gcloud components install beta --quiet',
+            'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\{} {} {} {} {}'.format(target['buildScript'], target['workerType'], os.environ.get('GITHUB_HEAD_REPO_URL', 'https://github.com/mozilla-platform-ops/relops-image-builder.git').split('/')[3], os.environ.get('GITHUB_HEAD_REPO_NAME', 'relops-image-builder'), os.environ.get('GITHUB_HEAD_SHA', 'master'))
+          ] if target['provider'] == 'gcp' else [
+            'git clone {} relops-image-builder'.format(os.environ.get('GITHUB_HEAD_REPO_URL')),
+            'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder config advice.detachedHead false',
+            'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder checkout {}'.format(os.environ.get('GITHUB_HEAD_SHA')),
+            'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\{} {} {} {} {}'.format(target['buildScript'], target['workerType'], os.environ.get('GITHUB_HEAD_REPO_URL', 'https://github.com/mozilla-platform-ops/relops-image-builder.git').split('/')[3], os.environ.get('GITHUB_HEAD_REPO_NAME', 'relops-image-builder'), os.environ.get('GITHUB_HEAD_SHA', 'master'))
+          ],
+          'features': {
+            'runAsAdministrator': True,
+            'taskclusterProxy': True
+          }
+        },
+        'metadata': {
+          'name': '{} :: {} :: {}'.format(target['provider'], target['workerType'], target['name']),
+          'description': '{} for {}'.format(target['decription'], target['workerType']),
+          'owner': os.environ.get('GITHUB_HEAD_USER_EMAIL'),
+          'source': '{}/commit/{}'.format(os.environ.get('GITHUB_HEAD_REPO_URL')[:-4], os.environ.get('GITHUB_HEAD_SHA')[0:7])
         }
-      ] if target['provider'] == 'ec2' else [],
-      'command': [
-        'git clone {} relops-image-builder'.format(os.environ.get('GITHUB_HEAD_REPO_URL')),
-        'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder config advice.detachedHead false',
-        'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder checkout {}'.format(os.environ.get('GITHUB_HEAD_SHA')),
-        'for /F "delims=" %%i in (\'"C:\\Program Files (x86)\\Google\\Cloud SDK\\google-cloud-sdk\\bin\\gcloud.cmd" components copy-bundled-python\') do (set CLOUDSDK_PYTHON=%%i)',
-        'gcloud components install beta --quiet',
-        'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\{} {} {} {} {}'.format(target['buildScript'], target['workerType'], os.environ.get('GITHUB_HEAD_REPO_URL', 'https://github.com/mozilla-platform-ops/relops-image-builder.git').split('/')[3], os.environ.get('GITHUB_HEAD_REPO_NAME', 'relops-image-builder'), os.environ.get('GITHUB_HEAD_SHA', 'master'))
-      ] if target['provider'] == 'gcp' else [
-        'git clone {} relops-image-builder'.format(os.environ.get('GITHUB_HEAD_REPO_URL')),
-        'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder config advice.detachedHead false',
-        'git --git-dir=.\\relops-image-builder\\.git --work-tree=.\\relops-image-builder checkout {}'.format(os.environ.get('GITHUB_HEAD_SHA')),
-        'powershell -NoProfile -InputFormat None -File .\\relops-image-builder\\{} {} {} {} {}'.format(target['buildScript'], target['workerType'], os.environ.get('GITHUB_HEAD_REPO_URL', 'https://github.com/mozilla-platform-ops/relops-image-builder.git').split('/')[3], os.environ.get('GITHUB_HEAD_REPO_NAME', 'relops-image-builder'), os.environ.get('GITHUB_HEAD_SHA', 'master'))
-      ],
-      'features': {
-        'runAsAdministrator': True,
-        'taskclusterProxy': True
       }
-    },
-    'metadata': {
-      'name': '{} :: {} :: {}'.format(target['provider'], target['workerType'], target['name']),
-      'description': '{} for {}'.format(target['decription'], target['workerType']),
-      'owner': os.environ.get('GITHUB_HEAD_USER_EMAIL'),
-      'source': '{}/commit/{}'.format(os.environ.get('GITHUB_HEAD_REPO_URL')[:-4], os.environ.get('GITHUB_HEAD_SHA')[0:7])
-    }
-  }
-  print('creating task {} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(target['taskId'], os.environ.get('TASK_ID'), target['taskId']))
-  taskStatusResponse = queue.createTask(target['taskId'], payload)
-  print(taskStatusResponse)
+      print('creating task: {}, for worker type: {} (https://tools.taskcluster.net/groups/{}/tasks/{})'.format(target['taskId'], target['workerType'], os.environ.get('TASK_ID'), target['taskId']))
+      taskStatusResponse = queue.createTask(target['taskId'], payload)
+      print(taskStatusResponse)
+    else:
+      print('no change detected in unattend file: {}, for worker type: {}. skipping build task.'.format(target['workerType'], os.path.basename(manifestItem['unattend'])))
+  else:
+    print('no mapping found for worker type: {}. skipping build task.'.format(target['workerType']))
 
 #for target in [t for t in targets if t['provider'] == 'gcp']:
 #  taskId = slugid.nice().decode('utf-8')
